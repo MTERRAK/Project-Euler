bf9b1972b94c2af5c17905efa665fe21349c7769
Assem Chelli <assem.ch@gmail.com>
2017-10-21 18:12:04 +0100

    Update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -6,4 +6,17 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 N = 1000
+
+-#Technique 1
+somme = 0
+for n in range(1, N): 
+   if n % 3 == 0 or n % 5 == 0:
+  		somme += n
+
+print somme
+
+#Technique 2
+print sum([n for n in range(1, N) if n % 3 ==0 or n % 5==0])
+
+#Technique 3
 print sum(set(xrange(0, N, 5)) | set(xrange(0, N, 3)))
